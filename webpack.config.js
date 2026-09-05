a291c647859f16792a67f2ee595d1200eab0fa6a
wangyi <vwangyi@foxmail.com>
2026-09-06 00:40:48 +0800

    webpack5-js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 /**
  * 
  */
-
+console.log(1)
 export default {
-    // entry: './src/index.js', 
+    entry: './src/index.js', 
+    output: {
+        clean: true,
+        filename: 'main.js'
+    }
 }
