b1059917d164461e9775bafd5f3932e851298dc4
WangYi <codewy@qq.com>
2026-09-07 18:28:36 +0800

    w

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
 /**
  * 
  */
-console.log(1)
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+
+
+console.log('path ', path.resolve(__dirname, './'))
 export default {
     entry: './src/index.js', 
     output: {
         clean: true,
         filename: 'main.js'
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './')
+        })
+    ]
 }
