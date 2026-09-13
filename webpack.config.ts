6e43778cf4351396e7a912b592e1901e3e9ab832
wangyi <vwangyi@foxmail.com>
2026-09-14 02:06:38 +0800

    webpack5

---
+++ webpack.config.ts
@@ -0,0 +1,19 @@
+const path = require('node:path');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                loader: './loader/xxx-loader.js',
+                options: {
+                    a: 1,
+                    b:2
+                }
+            }
+        ]
+    },
+}
